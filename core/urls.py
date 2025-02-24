5e080d815cd00cbdf25dd644e50207e68c4192f4
ClaudioFritzen <sdfritzen96@gmail.com>
2025-02-24 17:37:29 +0000

    fix(api): corrigido o erro 404 na rota principal

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,12 @@
-from .api import api
-
 from django.contrib import admin
 from django.urls import path, include
+from .api import api
+from treino.views import welcome_view
 from django.http import HttpResponseRedirect
-
-from django.contrib import admin
-from django.urls import path, include
-from .api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: HttpResponseRedirect('/api/')),  # Redireciona a URL base para /api
     path('api/', api.urls),  # Inclui as URLs da API
+    path('api/', welcome_view),  # Inclui as URLs da API
 ]
-
-
-""" urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', lambda request: HttpResponseRedirect('/api/')),  # Redireciona a URL base para /api
-    path('api/', include('api.urls')),  # Inclui as URLs do seu app no /api
-] """
--- treino/views.py
+++ treino/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Bem-vindo à minha API Ninja!")
+def welcome_view(request):
+    return HttpResponse("Bem-vindo à minha API Ninja! Acesse a documentação em /api/docs/")
